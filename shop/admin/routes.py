c28c66b0c788d3b2e0888400f7648b4776e8c1fc
juniorfelix998 <juniorfelix998@gmail.com>
2020-04-15 13:49:33 +0300

    added user login

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,14 +1,17 @@
 from flask import render_template,session,request,redirect,url_for,flash
 
 from shop import app,db,bcrypt
-from shop.admin.forms import RegistrationForm
+from shop.admin.forms import RegistrationForm,LoginForm
 from .models import User
 import os
 
 
 
 @app.route('/')
-def home():
+def admin():
+    if 'email' not in session:
+        flash(f'Please Login First','danger')
+        return redirect(url_for('login'))
     return render_template('admin/index.html',title='Admin page')
 
 
@@ -23,3 +26,18 @@ def register():
         flash(f'Welcome {form.name.data} Thanks you for registering','success')
         return redirect(url_for('home'))
     return render_template('admin/register.html',form=form,title='Registeration Page')
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            session['email']=form.email.data
+            flash(f'welcome{form.email.data} You are Loggedin Now','success')
+            return redirect(request.args.get('next') or url_for('admin'))
+        else:
+            flash('Wrong Password Please Try Again','danger')
+
+    return render_template('admin/login.html',form=form,title="Login Page")
