5f8aeb8aa2bf3e4edee8b783b009613ed3cb7356
juniorfelix998 <juniorfelix998@gmail.com>
2020-04-19 02:11:38 +0300

    added images upload

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,13 +2,24 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_uploads import IMAGES,UploadSet,configure_uploads,patch_request_class
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SECRET_KEY']='hfouewhfoiwefoquw'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir,'static/images')
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app,photos)
+patch_request_class(app) 
+
+
+
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,7 +1,11 @@
 from flask import redirect,render_template,url_for,flash,request
-from shop import db,app
+from shop import db,app,photos
 from .models import Brand,Category
 from .forms import Addproducts
+import secrets
+
+
+
 
 @app.route('/addbrand',methods=['GET','POST'])
 def addbrand():
@@ -33,4 +37,8 @@ def addproduct():
     brands = Brand.query.all()
     categories = Category.query.all()
     form = Addproducts(request.form)
+    if request.method == 'POST':
+        photos.save(request.files.get('image_1'),name=secrets.token_hex(10)+".")
+        photos.save(request.files.get('image_2'),name=secrets.token_hex(10)+".")
+        photos.save(request.files.get('image_3'),name=secrets.token_hex(10)+".")
     return render_template('products/addproduct.html',title="Add Product Page",form=form,brands=brands,categories=categories)
